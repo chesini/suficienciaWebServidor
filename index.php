e95cfe9fee42a9c6e6d42ff82ebc013f63e00e95
chesini <leandrochesi@hotmail.com>
2021-03-16 10:21:22 -0300

    initial commit

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+
+require __DIR__. '/library/vendor/autoload.php';
---
+++ library/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'leandrochesini/suficiencia-web-servidor',
+  ),
+  'versions' => 
+  array (
+    'leandrochesini/suficiencia-web-servidor' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
